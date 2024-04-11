a574421369c5cc34de41ed6aea9f99b90b95a0d6
Lieztz3r <166023655+Lieztz3r@users.noreply.github.com>
2024-04-11 10:01:35 -0400

    h

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ win.onload= function() {
  document.querySelector('.Loading-screen').style.display = 'flex';
 setTimeOut(function() {
    document.querySelector('.Loading-screen').style.display = 'none';
-   document.querySelector('.Content2').style.display = 'block';
+   document.querySelector('.content').style.display = 'block';
 }, 1000);
 };
