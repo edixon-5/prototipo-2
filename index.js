57a34b8b457771a745e91fbe11fe51d4a902187e
edixon-5 <126833808+edixon-5@users.noreply.github.com>
2024-04-25 09:37:29 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-win.onload= function() {
- document.querySelector('.Loading-screen').style.display = 'flex';
-setTimeOut(function() {
-   document.querySelector('.Loading-screen').style.display = 'none';
-   document.querySelector('.content').style.display = 'block';
-}, 1000);
-};
+
+
+
